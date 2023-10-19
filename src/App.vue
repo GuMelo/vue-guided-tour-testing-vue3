7fb9bfe4c88edf7924ee8740ddfe47dd6394cc48
Gustavo de Melo Corrente <gustavomelo-1@hotmail.com>
2023-10-19 12:22:28 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img class="step-2" alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <vue-guided-tour :steps="steps" />
 </template>
 
 <script>
@@ -10,6 +11,78 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      steps: [
+        {
+          target: '.step-1',
+          slot: 'step-1',
+          title: 'Testando o Tour',
+          content: 'Apenas um teste da lib que o Jeudi mandou no grupo. Nunca tinha usasdo, porém parece bem bacana!',
+          popover: {
+            placement: 'start',
+            position: 'right',
+            id: 'hello-world',
+          },
+        },
+        {
+          target: '.step-2',
+          title: 'Logo do Vue',
+          content: 'Aqui é a logo do Vue!',
+          popover: {
+            placement: 'end',
+            position: 'right',
+          },
+        },
+        {
+          target: '.step-3',
+          title: 'Bem-vindo ao Vue.js App',
+          content: 'Bem-vindo ao Vue.js App',
+          popover: {
+            placement: 'center',
+            position: 'left',
+          },
+        },
+        {
+          target: '.step-4',
+          title: 'Plugins Instalados',
+          content: 'Plugins Instalados',
+          popover: {
+            placement: 'center',
+            position: 'top',
+          },
+        },
+        {
+          target: '.step-5',
+          title: 'Links Importantes',
+          content: 'Links Importantes',
+          popover: {
+            placement: 'center',
+            position: 'top',
+          },
+        },
+        {
+          target: '.step-6',
+          title: 'Todo Ecossistema',
+          content: 'Todo Ecossistema',
+          popover: {
+            placement: 'center',
+            position: 'top',
+          },
+        },
+        {
+          target: '.step-7',
+          title: 'Muito Bem-vindo!',
+          content: 'Caso hajam mais dúvidas, visite nosso FAC.',
+          popover: {
+            placement: 'start',
+            position: 'center',
+            id: 'finish',
+          },
+        },
+      ],
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGuidedTour from 'vue-guided-tour'
+import 'vue-guided-tour/style.css'
 
-createApp(App).mount('#app')
+createApp(App).use(VueGuidedTour).mount('#app')
